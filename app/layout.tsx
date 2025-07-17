1723c5bb9ca02c24af46a0a65d1f8523f0851c72
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-07-18 00:30:59 +0530

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import { ReactNode } from "react";
 
+export const metadata = {
+  title: 'Junaid - Full Stack Developer',
+  description: 'Full Stack Developer specialized in React,Next.js, Node.js, and Cloud Technologies. Building web applications with modern technologies.',
+  openGraph: {
+    title: 'Junaid Shaikh - Full Stack Developer',
+    description: 'Full Stack Developer specialized in React,Next.js, Node.js, and Cloud Technologies. Building web applications with modern technologies.',
+    images: [{
+      url: '/preview.png',
+      width: 1200,
+      height: 630,
+      alt: 'Junaid Portfolio Preview'
+    }],
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://zjunaidz.dev',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Junaid Shaikh - Full Stack Developer',
+    description: 'Full Stack Developer specialized in React, Node.js, and Cloud Technologies.',
+    images: ['/preview.png'],
+  }
+};
+
 export default function RootLayout({
   children,
 }: {
